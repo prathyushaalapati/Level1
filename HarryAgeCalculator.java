e4f461af802f4e83c37b151004462b08540e5d89
pratyushaalapati <na0752@srmist.edu.in>
2026-02-23 17:22:04 +0530

    Keep only PenDistribution.java in feature/PenDistribution

--- HarryAgeCalculator.java
+++
@@ -1,9 +0,0 @@
-public class HarryAgeCalculator {
-
-    public static void main(String[] args) {
-        int birthYear = 2008;
-        int currentYear = 2026;
-        int harryAge = currentYear - birthYear;
-        System.out.println("Harry's age in " + currentYear + " is " + harryAge);
-    }
-}
